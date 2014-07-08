db2aefacd289964f031f40a9935d45e41b3de398
J7mbo <jm_cj@hotmail.com>
2014-07-08 16:36:10 +0100

    Added async for ClientAdapter::getTorrents() (Transmission)

--- src/Client/Transmission/ClientAdapter.php
+++ src/Client/Transmission/ClientAdapter.php
@@ -17,68 +17,22 @@ class ClientAdapter extends BaseClientAdapter
     /**
      * @see ClientTransport::getTorrents()
      */
-    public function getTorrents(array $ids = array())
+    public function getTorrents(array $ids = array(), callable $callable = null)
     {
-        $data = json_decode($this->transport->getTorrents($ids));
-
-        $torrents = array();
-
-        foreach ($data->arguments->torrents as $obj)
+        if ($callable === null)
         {
-            $torrent = $this->torrentFactory->build($obj->hashString, $obj->name, $obj->sizeWhenDone);
-            $torrent->setDownloadSpeed($obj->rateDownload);
-            $torrent->setUploadSpeed($obj->rateUpload);
-            $torrent->setErrorString($obj->errorString);
+            $data = $this->transport->getTorrents($ids);
 
-            /** Transmission uses integers for statuses **/
-            /** @see <https://trac.transmissionbt.com/browser/branches/2.4x/libtransmission/transmission.h> **/
-            switch ($obj->status)
-            {
-                case 0:
-                    $status = Torrent::STATUS_PAUSED;
-                break;
-                case 1:
-                    $status = 'Queued to check';
-                break;
-                case 2:
-                    $status = 'Checking';
-                break;
-                case 3:
-                    $status = 'Queued to download';
-                break;
-                case 4:
-                    $status = Torrent::STATUS_DOWNLOADING;
-                break;
-                case 5:
-                    $status = 'Queued to seed';
-                break;
-                case 6:
-                    $status = Torrent::STATUS_SEEDING;
-                break;
-                default:
-                    $status = 'Unknown';
-                break;
-            }
-
-            $torrent->setStatus($status);
-
-            /** Adding filesizes together for total size as $obj->downloadedEver is unreliable copmared with size **/
-            $bytesDownloaded = 0;
-
-            foreach ($obj->files as $fileData)
-            {
-                $torrent->addFile($this->fileFactory->build($fileData->name, $fileData->length));
-
-                $bytesDownloaded += $fileData->bytesCompleted;
-            }
-
-            $torrent->setBytesDownloaded($bytesDownloaded);
-            $torrent->setBytesUploaded($obj->uploadedEver);
-
-            $torrents[] = $torrent;
+            return $this->convertJsonToTorrentObjects($data);
         }
+        else
+        {
+            $jsonToTorrents = function($jsonResponse) use ($callable) {
+                $callable($this->convertJsonToTorrentObjects($jsonResponse));
+            };
 
-        return $torrents;
+            $this->transport->getTorrents($ids, $jsonToTorrents);
+        }
     }
 
     /**
@@ -131,5 +85,76 @@ class ClientAdapter extends BaseClientAdapter
         $data = json_decode($this->transport->deleteTorrent($torrent, $torrentId));
 
         return ($data->result === 'success');
+    }
+
+    /**
+     * Helper method used by both sync and async calls to convert json data of torrents into actual Torrent objects
+     *
+     * @param string $data JSON data from the client
+     *
+     * @return array
+     */
+    private function convertJsonToTorrentObjects($data)
+    {
+        $torrents = array();
+
+        $data = json_decode($data);
+
+        foreach ($data->arguments->torrents as $obj)
+        {
+            $torrent = $this->torrentFactory->build($obj->hashString, $obj->name, $obj->sizeWhenDone);
+            $torrent->setDownloadSpeed($obj->rateDownload);
+            $torrent->setUploadSpeed($obj->rateUpload);
+            $torrent->setErrorString($obj->errorString);
+
+            /** Transmission uses integers for statuses **/
+            /** @see <https://trac.transmissionbt.com/browser/branches/2.4x/libtransmission/transmission.h> **/
+            switch ($obj->status)
+            {
+                case 0:
+                    $status = Torrent::STATUS_PAUSED;
+                    break;
+                case 1:
+                    $status = 'Queued to check';
+                    break;
+                case 2:
+                    $status = 'Checking';
+                    break;
+                case 3:
+                    $status = 'Queued to download';
+                    break;
+                case 4:
+                    $status = Torrent::STATUS_DOWNLOADING;
+                    break;
+                case 5:
+                    $status = 'Queued to seed';
+                    break;
+                case 6:
+                    $status = Torrent::STATUS_SEEDING;
+                    break;
+                default:
+                    $status = 'Unknown';
+                    break;
+            }
+
+            $torrent->setStatus($status);
+
+            /** Adding filesizes together for total size as $obj->downloadedEver is unreliable copmared with size **/
+            $bytesDownloaded = 0;
+
+            foreach ($obj->files as $fileData)
+            {
+                $torrent->addFile($this->fileFactory->build($fileData->name, $fileData->length));
+
+                $bytesDownloaded += $fileData->bytesCompleted;
+            }
+
+            $torrent->setBytesDownloaded($bytesDownloaded);
+            $torrent->setBytesUploaded($obj->uploadedEver);
+
+            $torrents[] = $torrent;
+        }
+
+        return $torrents;
     }
 }
